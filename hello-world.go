0fc807951b8166871222ba29ff266008697a9417
Pragmatic Fermat <141491326+pragmatic-fermat@users.noreply.github.com>
2025-10-09 06:50:57 +0200

    Create hello-world.go

---
+++ hello-world.go
@@ -0,0 +1,41 @@
+package main
+
+// Imports (non nécessaires)
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+)
+
+func getIP() string {
+    // L'API est payante en HTTPS mais tjrs Free en HTTP (??)
+    response, err := http.Get("http://ip-api.com/json/?fields=query")
+
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return string(responseData)
+
+}
+
+func getVer() int {
+    version:= 116
+    return version
+}
+
+func main() {
+
+    fmt.Println("Return from ip-api.com: ")
+    fmt.Println(string(getIP()))
+
+    version := getVer()
+    fmt.Println(`(new) hello world version`,version)
+}
